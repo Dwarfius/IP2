36624e043a77bac0cdee4c6bafa877fc92df2293
Dwarfius <youman6@gmail.com>
2015-02-08 19:21:26 +0000

    vision cone, first version, not perfect

---
+++ Assets/Scripts/VisionCone.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionCone : MonoBehaviour 
+{
+    public float radius;
+    public float angle;
+
+	void Start () 
+    {
+        Mesh m = new Mesh();
+        Vector3 radV = new Vector3(radius, 0, 0);
+        Vector3[] vertices = { Vector3.zero,
+                               radV,
+                               Quaternion.Euler(0, 0, angle) * radV,
+                               Quaternion.Euler(0, 0, -angle) * radV };
+        int[] indices = { 0, 2, 1, 0, 1, 3 };
+        Vector2[] uvs = { Vector2.zero, Vector2.one, Vector2.one, Vector2.one };
+        m.vertices = vertices;
+        m.triangles = indices;
+        m.uv = uvs;
+        GetComponent<MeshFilter>().mesh = m;
+        GetComponent<MeshRenderer>().material.SetFloat("_Radius", radius + 0.42f);
+
+        PolygonCollider2D col = gameObject.AddComponent<PolygonCollider2D>();
+        Vector2[] points = { vertices[0], vertices[2], vertices[1], vertices[3] };
+        col.points = points;
+        col.isTrigger = true;
+	}
+}
---
+++ Assets/Shaders/VisionCone.shader
@@ -0,0 +1,46 @@
+﻿Shader "Custom/VisionCone" {
+	Properties {
+		_Color("Cone Color", Color) = (1,1,1,1)
+		_Radius("Cone Radius", float) = 0
+	}
+	SubShader {
+		Lighting Off
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+		LOD 200
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			float3 _Color;
+			float _Radius;
+
+			struct appdata {
+				float4 vertex : POSITION;
+			};
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			v2f vert(appdata v) : POSITION
+			{
+				v2f o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				float dist = length(v.vertex);
+				o.color = float4(_Color, (_Radius - dist)/_Radius); //not the best practive, but it works, and GPU doesn't care
+				return o;
+			}
+
+			half4 frag(v2f i) : COLOR
+			{
+				return i.color;
+			}
+			ENDCG
+		}
+	} 
+	FallBack Off
+}
