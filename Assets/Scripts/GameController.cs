aee437014d33ad78d23680a35fa73567b33d6de0
Dwarfius <youman6@gmail.com>
2015-02-10 15:51:30 +0000

    Added a game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+    public GameObject playerPrefab;
+    public string spawnTag;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if(level != 0)
+        {
+            GameObject spawn = GameObject.FindGameObjectWithTag(spawnTag);
+            GameObject.Instantiate(playerPrefab, spawn.transform.position, Quaternion.identity);
+        }
+    }
+    
+    public void LoadLevel(int i)
+    {
+        Application.LoadLevel(i);
+    }
+}
