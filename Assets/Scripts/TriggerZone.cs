d3bdf96c50465448b810d58ca6d68dcca99404d1
Dwarfius <youman6@gmail.com>
2015-02-10 15:27:19 +0000

    Added Trigger Zones

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class TriggerZone : MonoBehaviour 
+{
+    public List<string> tagsToReactTo;
+    public UnityEvent onEnter, onExit;
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(tagsToReactTo.Contains(other.tag))
+        {
+            onEnter.Invoke();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(tagsToReactTo.Contains(other.tag))
+        {
+            onExit.Invoke();
+        }
+    }
+}
