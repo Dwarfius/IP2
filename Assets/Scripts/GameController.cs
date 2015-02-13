79685cd601478cad60ec6ec21b8ce46fee25935b
Johnm95 <john_m_95@hotmail.com>
2015-02-13 16:42:18 +0000

    Johns scene
    I added basic movement, a pickup, struggling to get it to work the way
    intended.  Message displays as scene runs but only supposed to display
    when player enters tag.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,57 @@ public class GameController : MonoBehaviour
 {
     public GameObject playerPrefab;
     public string spawnTag;
+	//
+	public float playerVelocity;
+	public Vector2 playerPosition;
+	public float boundary;
+	public bool messagePopup;
+
+
+
 
     void Start()
     {
+		playerPosition=gameObject.transform.position;
         DontDestroyOnLoad(gameObject);
     }
+
+	void Update()
+	{
+		playerPosition.x += Input.GetAxis("Horizontal")*playerVelocity;
+		playerPosition.y += Input.GetAxis("Vertical")*playerVelocity;
+		 
+		transform.position = playerPosition;
+
+		if (playerPosition.x <- boundary)
+		{
+			transform.position = new Vector2(-boundary,playerPosition.y);
+		}
+		if (playerPosition.x > boundary)
+		{
+			transform.position = new Vector2(boundary,playerPosition.y);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		messagePopup = true;
+		if (col.gameObject.tag=="Pickup")
+		{
+
+			Destroy (col.gameObject);
+		}
+
+	}
+
+	void OnGUI()
+	{
+		if (messagePopup = true)
+		{
+			GUI.Label(new Rect(20,20,100,50),"Press A to pick up!");
+		}
+		messagePopup = false;
+	}
 
     void OnLevelWasLoaded(int level)
     {
