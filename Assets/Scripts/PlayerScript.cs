fdd33ebcd71c393c74b54e6a2993744a1862e3d1
Dwarfius <youman6@gmail.com>
2015-03-01 18:18:25 +0000

    Partial completion of the tutorial level
    Added enemy patrols
    Interactible Doors
    Collidable Walls
    Working pressure playes
    Pickable items

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,123 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
-
-	//variable for the speed of the player movement
-	public float playerVelocity;
-	//variable for the position of the player
-	public Vector2 playerPosition;
-	//variable for the boundary to prevent the player going out of the game
-	public float boundary;
-	//variable to store the pop up message when player hits a pick up
-	public bool messagePopup;
-	//variable which stores the text that is contained in the messagePopup
-	public string labelText = "";
-	//variable which declares the coin game object
-	GameObject coin;
-	//variable which declares the Player game object
-	public GameObject Player;
-	//variable which declares the players health, with a start value of 100
-	//public int health = 100;
-	public GameObject Enemy;
+public class PlayerScript : MonoBehaviour 
+{
+    public float playerVelocity;
+    public int pickupCount;
 	public string PickupTag = "Pickup";
-	private Animator animator;
-	 
+	
+    Animator animator;
+    GameObject coin;
+    bool messagePopup;
+    Vector2 playerPosition;
+    string labelText = "";
 
-	public int pickupCount;
-	GameObject enemy;
-
-	GameObject Button;
-
-	// Use this for initialization
-	void Start () {
-
-		playerPosition=gameObject.transform.position;
-		DontDestroyOnLoad(gameObject);
-		//.
+	void Start () 
+    {
 		animator = GetComponent<Animator>();
-
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-		playerPosition.x += Input.GetAxis("Horizontal")*playerVelocity;
-		playerPosition.y += Input.GetAxis("Vertical")*playerVelocity;
-		
-		transform.position = playerPosition;
-
-		if (playerPosition.x <- boundary)
-		{
-			transform.position = new Vector2(-boundary,playerPosition.y);
-		}
-		if (playerPosition.x > boundary)
-		{
-			transform.position = new Vector2(boundary,playerPosition.y);
-		}
+	void Update () 
+    {
+        playerPosition = transform.position;
+		playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity * Time.deltaTime;
+        playerPosition.y += Input.GetAxis("Vertical") * playerVelocity * Time.deltaTime;
+        rigidbody2D.MovePosition(playerPosition);
 
 		if (Input.GetKeyDown(KeyCode.Space) && coin)
 		{
 			Destroy(coin);
 			messagePopup = false;
-		}
-		pickupCount++;
-
-		if (pickupCount == 3 && enemy)
-		{
-			Destroy(enemy);
+            pickupCount++;
 		}
 	}
-
-		
-
 	
 	void OnTriggerEnter2D (Collider2D col)
 	{
-		Debug.Log (col.gameObject.name);
 		if (col.gameObject.tag == PickupTag)
 		{
-			
 			messagePopup = true;
 			labelText = "Press Space to pickup";
 			coin = col.gameObject;
-
-			
 		}
 
-
-		if (col.gameObject.name=="Cone")
-		{
-			Application.Quit();
-		}
-		/*
-		if (col.gameObject.tag == enemy)
-		{
-			enemy = col.gameObject;
-		}
-		*/
+        if (col.gameObject.tag == "Enemy")
+            Application.LoadLevel(Application.loadedLevel);
 
 		if (col.gameObject.name=="Button")
-		{
-			animator.SetInteger("AnimState",1);
-		}
-	
+			animator.SetInteger("AnimState", 1);
 	}
 
 	void OnGUI()
-		
 	{
 		if (messagePopup)
-		{
-			GUI.Box(new Rect(140,Screen.height-50,Screen.width-300,120),(labelText));
-		}
-		
-		//GUI.Label (new Rect(20,20,200,80), "Health: "+health.ToString());
-		
-	}
-
-	void OnCollisionEnter2D(Collision2D col)
-	{
-
+			GUI.Box(new Rect(140,Screen.height-50,Screen.width-300,120), labelText);
 	}
 }
--- Assets/Scripts/TriggerZone.cs
+++ Assets/Scripts/TriggerZone.cs
@@ -5,18 +5,29 @@ using System.Collections.Generic;
 
 public class TriggerZone : MonoBehaviour 
 {
+    public bool fireOnce;
     public List<string> tagsToReactTo;
     public UnityEvent onEnter, onExit;
+
+    bool enterFired, exitFired;
 	
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(tagsToReactTo.Contains(other.tag))
+        if (!enterFired && tagsToReactTo.Contains(other.tag))
+        {
             onEnter.Invoke();
+            if (fireOnce) 
+                enterFired = true;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if(tagsToReactTo.Contains(other.tag))
+        if (!exitFired && tagsToReactTo.Contains(other.tag))
+        {
             onExit.Invoke();
+            if (fireOnce) 
+                exitFired = true;
+        }
     }
 }
